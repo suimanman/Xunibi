f06d6ccc148d2ed91998b3ba10dd60670ae66301
suimanman <wmc19831983050@126.com>
2024-10-17 09:47:39 +0800

    init

---
+++ src/main/java/com/example/xunibibackend/XunibiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.xunibibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XunibiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XunibiBackendApplication.class, args);
+    }
+
+}
