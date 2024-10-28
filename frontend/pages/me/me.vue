2391f9103a21e6531b1e5226299c5416bcfba5d9
suimanman <wmc19831983050@126.com>
2024-10-28 19:18:05 +0800

    小程序前端我的页面样式完成---10.28-1

--- frontend/pages/me/me.vue
+++ frontend/pages/me/me.vue
@@ -1,31 +1,48 @@
 <template>
-	<view v-if="!isLogin" class="myself">
-		<!-- <u--form labelPosition="left" :model="model1" :rules="rules" ref="uForm">
-			<u-form-item label="姓名" prop="userInfo.name" borderBottom ref="item1">
-				<u--input v-model="model1.userInfo.name" border="none">
-
-				</u--input>
-			</u-form-item>
-		</u--form> -->
-		<u-button @click="login" class="custom-style" type="success" shape="circle" size="large">点击登录</u-button>
-	</view>
-	<view v-else class="profile-page">
-		<!-- Centered Avatar -->
-		<view class="avatar-section">
-			<u-avatar class="avatar" size="100" src="/static/avatar.png"></u-avatar>
-			<text class="edit-avatar">编辑头像</text>
+	<view class="me">
+		<view v-if="!isLogin" class="contaier">
+			<!-- 页面装饰图片 -->
+			<image class="img-a" src="https://zhoukaiwen.com/img/loginImg/2.png"></image>
+			<image class="img-b" src="https://zhoukaiwen.com/img/loginImg/3.png"></image>
+			<!-- 标题 -->
+			<view class="t-b">{{ title }}</view>
+			<view class="t-b2">欢迎使用，虚拟币服务小程序</view>
+			<form class="cl">
+				<view class="t-a">
+					<image src="/static/icon/userId.png"></image>
+					<view class="line"></view>
+					<input type="number" placeholder="请输入学号" maxlength="6" v-model="userId" />
+				</view>
+				<view class="t-a">
+					<image src="/static/icon/password.png"></image>
+					<view class="line"></view>
+					<input type="number" name="password" password=true placeholder="请输入密码" v-model="password" />
+				</view>
+				<button @tap="login()">登 录</button>
+				<button class="register" @tap="register()">注 册</button>
+			</form>
+			<!-- <view class="t-f"><text>————— 第三方账号登录 —————</text></view>
+			 		<view class="t-e cl">
+			 			<view class="t-g" @tap="wxLogin()"><image src="https://zhoukaiwen.com/img/loginImg/wx.png"></image></view>
+			 			<view class="t-g" @tap="zfbLogin()"><image src="https://zhoukaiwen.com/img/loginImg/qq.png"></image></view>
+			 		</view> -->
 		</view>
+		<view v-else class="profile-page">
 
-		<u-cell-group class="cell-group">
-			<u-cell title="用户名" value="河工大不知名学生" is-link @click="goEditUsername" />
-			<u-cell title="修改密码" is-link @click="goEditUsername" />
-			<u-cell title="人脸照片上传" @click="uploadPicture" />
-			<u-cell title="所属团队" value="河工大" />
-		</u-cell-group>
+			<image class="img-b" src="https://zhoukaiwen.com/img/loginImg/3.png"></image>
+			<!-- Centered Avatar -->
+			<view class="avatar-section">
+				<u-avatar class="avatar" size="100" src="/static/avatar.png"></u-avatar>
+				<text class="edit-avatar">编辑头像</text>
+			</view>
 
-		<!-- Logout Button -->
-		<view class="logout-section">
-			<u-button class="custom-style" type="success" shape="circle" size="large" @click="logout">退出登录</u-button>
+			<u-cell-group class="cell-group">
+				<u-cell title="用户名" value="河工大不知名学生" is-link @click="goEditUsername" />
+				<u-cell title="修改密码" is-link @click="goEditUsername" />
+				<u-cell title="人脸照片上传" @click="uploadPicture" />
+				<u-cell title="所属团队" value="河工大" />
+			</u-cell-group>
+			<button @tap="login()">退 出 登 录</button>
 		</view>
 	</view>
 </template>
@@ -34,15 +51,15 @@
 	export default {
 		data() {
 			return {
+				title: "众创空间",
 				isLogin: false,
-				// rules: {
-				// 	'userInfo.name': {
-				// 		type: 'string',
-				// 		required: true,
-				// 		message: '请填写姓名',
-				// 		trigger: ['blur', 'change']
-				// 	},
-				// }
+				login: {
+					user: {
+						userId: '',
+						password: '',
+					}
+
+				}
 			}
 		},
 		methods: {
@@ -60,19 +77,12 @@
 				// Handle logout
 			}
 		},
-		// onReady() {
-		//     	this.$refs.uForm.setRules(this.rules)
-		//     },
 	};
 </script>
 
-<style scoped>
-	.myself {
-		padding: 50px;
-		display: flex;
-		justify-content: center;
-		/* 水平居中 */
-		margin-top: 200px;
+<style lang="scss" scoped>
+	.contaier {
+		height: 100vh;
 	}
 
 	.cell-group {
@@ -97,16 +107,157 @@
 		color: #666;
 	}
 
-	.logout-section {
-		margin: 30px 100px;
-		display: flex;
-		justify-content: center;
-	}
-
 	.custom-style {
 		width: 100px;
 		color: #f3f3f3;
 		border-radius: 30px;
 		border: none;
+	}
+
+	.img-a {
+		position: absolute;
+		width: 100%;
+		top: -150rpx;
+		right: 0;
+	}
+
+	.img-b {
+		position: absolute;
+		width: 50%;
+		bottom: 0;
+		left: -50rpx;
+		/* margin-bottom: -200rpx; */
+	}
+
+	.me {
+		width: 650rpx;
+		margin: 0 auto;
+		font-size: 28rpx;
+		color: #000;
+	}
+
+	.me button {
+		font-size: 28rpx;
+		background: #5677fc;
+		color: #fff;
+		height: 90rpx;
+		line-height: 90rpx;
+		border-radius: 50rpx;
+		box-shadow: 0 5px 7px 0 rgba(86, 119, 252, 0.2);
+	}
+
+	.me input {
+		padding: 0 20rpx 0 120rpx;
+		height: 90rpx;
+		line-height: 90rpx;
+		margin-bottom: 50rpx;
+		background: #f8f7fc;
+		border: 1px solid #e9e9e9;
+		font-size: 28rpx;
+		border-radius: 50rpx;
+	}
+
+	.me .t-a {
+		position: relative;
+	}
+
+	.me .t-a image {
+		width: 40rpx;
+		height: 40rpx;
+		position: absolute;
+		left: 40rpx;
+		top: 28rpx;
+		/* border-right: 2rpx solid #dedede; */
+		margin-right: 20rpx;
+	}
+
+	.me .t-a .line {
+		width: 2rpx;
+		height: 40rpx;
+		background-color: #dedede;
+		position: absolute;
+		top: 28rpx;
+		left: 98rpx;
+	}
+
+	.me .t-b {
+		text-align: left;
+		font-size: 46rpx;
+		color: #000;
+		padding: 300rpx 0 30rpx 0;
+		font-weight: bold;
+	}
+
+	.me .t-b2 {
+		text-align: left;
+		font-size: 32rpx;
+		color: #aaaaaa;
+		padding: 0rpx 0 120rpx 0;
+	}
+
+	.me .t-c {
+		position: absolute;
+		right: 22rpx;
+		top: 22rpx;
+		background: #5677fc;
+		color: #fff;
+		font-size: 24rpx;
+		border-radius: 50rpx;
+		height: 50rpx;
+		line-height: 50rpx;
+		padding: 0 25rpx;
+	}
+
+	.me .t-d {
+		text-align: center;
+		color: #999;
+		margin: 80rpx 0;
+	}
+
+	.me .t-e {
+		text-align: center;
+		width: 250rpx;
+		margin: 80rpx auto 0;
+	}
+
+	.me .t-g {
+		float: left;
+		width: 50%;
+	}
+
+	.me .t-e image {
+		width: 50rpx;
+		height: 50rpx;
+	}
+
+	.me .t-f {
+		text-align: center;
+		margin: 200rpx 0 0 0;
+		color: #666;
+	}
+
+	.me .t-f text {
+		margin-left: 20rpx;
+		color: #aaaaaa;
+		font-size: 27rpx;
+	}
+
+	.me .uni-input-placeholder {
+		color: #000;
+	}
+
+	.cl {
+		zoom: 1;
+	}
+
+	.cl:after {
+		clear: both;
+		display: block;
+		visibility: hidden;
+		height: 0;
+		content: '\20';
+	}
+	.register{
+		top: 10px;
 	}
 </style>
